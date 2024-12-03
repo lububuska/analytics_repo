7a3988734689eea0c14ce1a5fd220415990bba31
lububuska <119815794+lububuska@users.noreply.github.com>
2024-12-04 02:04:37 +0300

    style: reformatted code using clang-format wih Mozilla style

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-using namespace std;
-int main() {
+int
+main()
+{
 	//Переменная для имени пользователя
 	std::string name;
 	//Вводим имя пользователя
-	std::cout <<"Enter your name: ";
+	std::cout << "Enter your name: ";
 	std::cin >> name;
 	//Выводим приветствие и имя пользователя
-	std::cout << "Hello World from " << name << "!" << std::endl;
+	std::cout << "Hello world from " << name << "!" << std::endl;
 	return 0;
 }
