178b8b9d1dd4aa388270637c6fe7acfa551b8fe4
lububuska <119815794+lububuska@users.noreply.github.com>
2024-12-04 01:15:35 +0300

    feat: add user input for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 using namespace std;
 int main() {
-	cout << "Hello World!" << endl;
+	string name;
+	cout << "Enter your name:" << endl;
+	cin >> name;
+	cout << "Hello World from " << name << "!" << endl;
 	return 0;
 }
