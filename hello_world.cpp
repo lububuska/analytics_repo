7a70787a8a99dcdd0278f3a958260dbd89a900bf
lububuska <119815794+lububuska@users.noreply.github.com>
2024-12-04 01:32:45 +0300

    docs: add comments to the code

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
+	//Переменная для имени пользователя
 	std::string name;
+	//Вводим имя пользователя
 	std::cout <<"Enter your name: ";
 	std::cin >> name;
+	//Выводим приветствие и имя пользователя
 	std::cout << "Hello World from " << name << "!" << std::endl;
 	return 0;
 }
